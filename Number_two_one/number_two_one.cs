9cf76d456b2e809bd74db78255b3e7dac8e36c0b
kenuel0110 <zeroonezero0110@gmail.com>
2024-02-08 00:25:16 +0300

    number

--- Number_two_one/number_two_one.cs
+++ Number_two_one/number_two_one.cs
@@ -6,7 +6,7 @@ namespace Number_two_one
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wofgd!");
         }
     }
 }
